0cab24e25337c67ec90b8b1fd399654a04e43ef4
lucasbmiotto <lucas.b.miotto@hotmail.com>
2022-10-03 12:03:04 -0300

    Definição da janela

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-CalculadoraPython
+# BIBLIOTECAS
+from tkinter import *
+from tkinter import ttk
+
+# CONFIGURAÇÃO DA JANELA
+janela = Tk()
+janela.title('CALCULADORA')
+janela.geometry('235x318')
+janela.configure(bg="#feffff")
+
+
+style = ttk.Style(janela)
+style.theme_use("clam")
