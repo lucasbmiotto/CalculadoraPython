0498b97d0be19e8c8ab680cfa181a96f624ee6fd
lucasbmiotto <lucas.b.miotto@hotmail.com>
2022-10-03 12:14:38 -0300

    Configurando os frames

--- main.py
+++ main.py
@@ -2,12 +2,29 @@
 from tkinter import *
 from tkinter import ttk
 
+# VARIAVEIS COM PELETA DE CORES
+branco = "#feffff"
+laranja = '#FFAB40'
+bege ="#ab8918"
+cinza ="#ECEFF1"
+back = "#3b3b3b"
+
 # CONFIGURAÇÃO DA JANELA
 janela = Tk()
 janela.title('CALCULADORA')
 janela.geometry('235x318')
-janela.configure(bg="#feffff")
-
+janela.configure(bg=branco)
 
 style = ttk.Style(janela)
 style.theme_use("clam")
+
+# DEFINIÇÃO DOS FRAMES
+ttk.Separator(janela, orient=HORIZONTAL).grid(row=0, columnspan=1, ipadx=280)
+
+frame_score = Frame(janela, width=300, height=56,bg=branco, pady=0, padx=0, relief="flat",)
+frame_score.grid(row=1, column=0, sticky=NW)
+
+frame_quadros = Frame(janela, width=300, height=340,bg=back, pady=0, padx=0, relief="flat",)
+frame_quadros.grid(row=2, column=0, sticky=NW)
+
+janela.mainloop()
