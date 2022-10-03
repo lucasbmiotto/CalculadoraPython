665ca6fa59a22d5def9dbd96ee16485aece3d3a3
lucasbmiotto <lucas.b.miotto@hotmail.com>
2022-10-03 14:36:38 -0300

    Botões de números

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
-# BIBLIOTECAS
 from tkinter import *
 from tkinter import ttk
 
 # VARIAVEIS COM PELETA DE CORES
 branco = "#feffff"
 laranja = '#FFAB40'
-bege ="#ab8918"
 cinza ="#ECEFF1"
 back = "#3b3b3b"
 
 # CONFIGURAÇÃO DA JANELA
 janela = Tk()
-janela.title('CALCULADORA')
+janela.title('Calculadora')
 janela.geometry('235x318')
 janela.configure(bg=branco)
 
@@ -27,19 +25,21 @@ frame_score.grid(row=1, column=0, sticky=NW)
 frame_quadros = Frame(janela, width=300, height=340,bg=back, pady=0, padx=0, relief="flat",)
 frame_quadros.grid(row=2, column=0, sticky=NW)
 
-# FUNÇÕES DA CALCULADORA
+
+################# Funções ####################
+
 def entrada_valores(event):
-    global all_values
+	global all_values
 	all_values = all_values + str(event)
 	value_text.set(all_values)
 
 def calculo():
-    global all_values
+	global all_values
 	result = str(eval(all_values))
 	value_text.set(result)
 	all_values = ""
 
-def limpar_valores():
+def limpar_valores(): 
     global all_values
     all_values = "" 
     value_text.set("")
@@ -47,7 +47,36 @@ def limpar_valores():
 # ARMAZENAMENTO DOS CALCULOS USADOS
 all_values = "" 
 value_text = StringVar()
-# BOTÕES
 
+################# Label ####################
+
+app_scream = Label(frame_score,width=16,height=2,textvariable = value_text , padx=7, relief="flat", anchor="e",bd=0, justify=RIGHT, font=('Ivy 18 '), bg='#37474F', fg=branco)
+app_scream.place(x=0, y=0)
+
+# BOTÕES
+tela = Label(frame_score,width=16,height=2,textvariable = value_text , padx=7, relief="flat", anchor="e",bd=0, justify=RIGHT, font=('Ivy 18 '), bg='#37474F', fg=branco)
+tela.place(x=0, y=0)
+
+# NUMERICOS
+b0 = Button(frame_quadros, text="0", width=11, height=2, bg=cinza, fg=back,font=('Ivy 12 bold'),relief=RAISED, overrelief=RIDGE,command = lambda: entrada_valores(0))
+b0.place(x=0, y=208)
+b1 = Button(frame_quadros, text="1", width=5, height=2, bg=cinza, fg=back,font=('Ivy 12 bold'),relief=RAISED, overrelief=RIDGE,command = lambda: entrada_valores(1))
+b1.place(x=0, y=156)
+b2 = Button(frame_quadros, text="2", width=5, height=2, bg=cinza, fg=back,font=('Ivy 12 bold'),relief=RAISED, overrelief=RIDGE,command = lambda: entrada_valores(2))
+b2.place(x=59, y=156)
+b3 = Button(frame_quadros, text="3", width=5, height=2, bg=cinza, fg=back,font=('Ivy 12 bold'),relief=RAISED, overrelief=RIDGE,command = lambda: entrada_valores(3))
+b3.place(x=118, y=156)
+b4 = Button(frame_quadros, text="4", width=5, height=2, bg=cinza, fg=back,font=('Ivy 12 bold'),relief=RAISED, overrelief=RIDGE,command = lambda: entrada_valores(4))
+b4.place(x=0, y=104)
+b5 = Button(frame_quadros, text="5", width=5, height=2, bg=cinza, fg=back,font=('Ivy 12 bold'),relief=RAISED, overrelief=RIDGE,command = lambda: entrada_valores(5))
+b5.place(x=59, y=104)
+b6 = Button(frame_quadros, text="6", width=5, height=2, bg=cinza, fg=back,font=('Ivy 12 bold'),relief=RAISED, overrelief=RIDGE,command = lambda: entrada_valores(6))
+b6.place(x=118, y=104)
+b7 = Button(frame_quadros, text="7", width=5, height=2, bg=cinza, fg=back,font=('Ivy 12 bold'),relief=RAISED, overrelief=RIDGE,command = lambda: entrada_valores(7))
+b7.place(x=0, y=52)
+b8 = Button(frame_quadros, text="8", width=5, height=2, bg=cinza, fg=back,font=('Ivy 12 bold'),relief=RAISED, overrelief=RIDGE,command = lambda: entrada_valores(8))
+b8.place(x=59, y=52)
+b9 = Button(frame_quadros, text="9", width=5, height=2, bg=cinza, fg=back,font=('Ivy 12 bold'),relief=RAISED, overrelief=RIDGE,command = lambda: entrada_valores(9))
+b9.place(x=118, y=52)
 
 janela.mainloop()
